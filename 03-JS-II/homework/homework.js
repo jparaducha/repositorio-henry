5bd257b329fb8a77a98a411c749db2df8dd36796
jparaducha <68435382+jparaducha@users.noreply.github.com>
2021-08-05 17:53:27 -0300

    updated JSII homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -64,7 +64,7 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero == 10 || 5) return true;
+  if(numero == 10 || numero==5) return true;
   return false;
 }
 
@@ -119,12 +119,13 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero<2) return false;
   for(let i = 2; i<Math.sqrt(numero);i++){
-    if(numero%i==0){
-      return true;
+    if(numero%i==0 && numero!=i){
+      return false;
     }
   }
-  return false;
+  return true;
 }
 
 function esVerdadero(valor){
@@ -149,7 +150,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero>99) return true;
+  if((numero>99 && numero<1000) ||(numero>-1000 && numero<-99)) return true;
   return false;
 }
 
@@ -160,6 +161,7 @@ function doWhile(numero) {
   let i = 0;
   do{
     numero+=5;
+    i++;
   }while(i<8);
 
   return numero;
